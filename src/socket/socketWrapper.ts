920d73d0b1b3303015ec1e84c101752d0d74a6df
Vincent Landgraf <vincent@pacetelematics.com>
2020-05-07 14:01:12 +0200

    forward (UN)LOCKPUMP to the session

--- src/socket/socketWrapper.ts
+++ src/socket/socketWrapper.ts
@@ -142,7 +142,7 @@ class SocketWrapper {
         this.sendResponse(tag, FSC_API_CLIENT_RESPONSE.OK);
         break;
       case FSC_API_SERVER_REQUEST.LOCKPUMP:
-        // not implemented yet
+          session.handleLockPumpRequest(tag, args[0]);
         break;
       case FSC_API_SERVER_REQUEST.PRICES:
         session.handlePricesRequest(tag);
@@ -157,7 +157,7 @@ class SocketWrapper {
         session.handleTransactionsRequest(tag, args[0], args[1]);
         break;
       case FSC_API_SERVER_REQUEST.UNLOCKPUMP:
-        // not implemented yet
+        session.handleUnlockPumpRequest(tag, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11]);
         break;
       default:
         customConsole.warn(`[ WEB SOCKET REQUEST DOES NOT EXIST ] | method: '${method}'`);
